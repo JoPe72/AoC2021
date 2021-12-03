8939db7697470f9f5f0444c513541405e9bba0a9
Johan Persson <johanpersson@outlook.com>
2021-12-03 23:18:53 +0100

    Starting but too tired :-)

---
+++ Day 3/day3.ps1
@@ -0,0 +1,14 @@
+$diagnostics = Get-Content C:\Users\JohanPersson\OneDrive\Github\AoC2021\Day3\test-Input-day3.txt
+
+$gamma_rate = 0
+$epsilon_rate = 0
+
+
+
+
+
+
+
+
+
+$power_consumption = $gamma_rate * $epsilon_rate
